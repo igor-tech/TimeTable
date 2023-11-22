41c12c5ceb4c52665a406187448b9db1cd780a2a
Igor Shargin <igor20513@gmail.com>
2023-11-23 00:48:02 +0500

    fix: small fixes

--- src/components/modal/ClassRoomHoursModal/ClassRoomHoursModal.tsx
+++ src/components/modal/ClassRoomHoursModal/ClassRoomHoursModal.tsx
@@ -1,11 +1,15 @@
 import { FC } from 'react'
 
 import { CLASS_ROOM_HOURS_DATA } from '@/components/config.ts'
-import { Table } from '@mantine/core'
+import { CSSProperties, Table } from '@mantine/core'
 
 import styles from './ClassRoomHoursModal.module.css'
 
 export const ClassRoomHoursModal: FC = () => {
+  const TheadStyles = { th: { textAlign: 'center', whiteSpace: 'nowrap' } } as Partial<
+    Record<'th', CSSProperties>
+  >
+
   return (
     <>
       <Table
@@ -16,11 +20,11 @@ export const ClassRoomHoursModal: FC = () => {
         <Table.Thead>
           <Table.Tr>
             <Table.Th></Table.Th>
-            <Table.Th styles={{ th: { textAlign: 'center' } }}>1 занятие</Table.Th>
-            <Table.Th styles={{ th: { textAlign: 'center' } }}>2 занятие</Table.Th>
+            <Table.Th styles={TheadStyles}>1 занятие</Table.Th>
+            <Table.Th styles={TheadStyles}>2 занятие</Table.Th>
           </Table.Tr>
         </Table.Thead>
-        <Table.Tbody mt={'80px'}>
+        <Table.Tbody>
           {CLASS_ROOM_HOURS_DATA.map(element => (
             <Table.Tr key={element.lesson}>
               <Table.Td>{element.lesson}</Table.Td>
