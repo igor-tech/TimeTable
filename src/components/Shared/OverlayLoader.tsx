ff4e7d07ead89ac28e107ae02cfa5e68e227ccf5
Igor Shargin <igor20513@gmail.com>
2023-11-26 18:51:37 +0500

    fix: small fixes

--- src/components/Shared/OverlayLoader.tsx
+++ src/components/Shared/OverlayLoader.tsx
@@ -6,7 +6,7 @@ export const OverlayLoader: FC = () => {
   return (
     <LoadingOverlay
       loaderProps={{ type: 'bars' }}
-      overlayProps={{ blur: 1, radius: 'md', opacity: 0.6 }}
+      overlayProps={{ blur: 1, radius: 'md', opacity: 0.6, fixed: true }}
       styles={{ root: { scrollbarGutter: 'auto' } }}
       visible
       zIndex={1000}
