b12e5777f0821b99d1b111bcf88c9d6036e63efe
Igor Shargin <igor20513@gmail.com>
2023-11-26 20:48:18 +0500

    fix: small fixes

--- src/pages/Hero/HeroPage.tsx
+++ src/pages/Hero/HeroPage.tsx
@@ -74,7 +74,7 @@ export default function HeroPage() {
               className={classes.control}
               onClick={onClickButtonHandler}
               radius={'xl'}
-              size={'md'}
+              size={'xl'}
             >
               Начать использовать
             </Button>
